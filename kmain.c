40158e189d9e6d4dd36a5cbb4c40e5217595c009
rohitJung <rokshh28@gmail.com>
2026-07-14 03:10:58 +0545

    feat: message printing to framebuffer

--- kmain.c
+++ kmain.c
@@ -4,8 +4,11 @@
 
 int kmain()
 {
+	char message[] = "Sleep rokshh";
+
 	fb_move_cursor(6*80);
-	fb_write_simple();
+	/* fb_write_simple(); */
+	fb_write_str(message, sizeof(message));
 	return 0;
 }
 
