4060c6b5031954ef79f583ba5cfa1d2e2731672c
rohitJung <rokshh28@gmail.com>
2026-07-14 02:56:01 +0545

    feat: call the C code from asm

--- loader.s
+++ loader.s
@@ -1,17 +1,20 @@
 global  loader                   ; the entry symbol for ELF
-	MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
-	FLAGS        equ 0x0						; multiboot flags
-	CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
+extern  kmain
+
+MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
+FLAGS        equ 0x0						; multiboot flags
+CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
 	                                ;        (magic number + checksum + flags should equal 0)
 
 section .text:										  ; start of the text (code) section
-	align   4													; the code must be 4 byte aligned
+align   4													  ; the code must be 4 byte aligned
 	dd      MAGIC_NUMBER							; write the magic number to the machine code
 	dd      FLAGS											; the flags
 	dd      CHECKSUM									; and the check sum
 
 loader: 														; loader label defined as the entry point in script
 	mov eax, 0xCAFEBABE               ; place the number 0xCAFEBABE in the eax register
+  call kmain
 
 .loop:
 	jmp .loop                         ; loop forever
